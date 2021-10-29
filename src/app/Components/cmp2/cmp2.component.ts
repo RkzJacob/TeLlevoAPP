d3b9c58d9e58aafeed8d384d0cb79f2f1c59eb28
RkzJacob <jaco.ponce@duocuc.cl>
2021-10-29 04:58:28 -0300

    dependencias

--- src/app/Components/cmp2/cmp2.component.ts
+++ src/app/Components/cmp2/cmp2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BdLocalService } from 'src/app/services/bd-local.service';
 
 @Component({
   selector: 'app-cmp2',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cmp2.component.scss'],
 })
 export class Cmp2Component implements OnInit {
-
-  constructor() { }
-
+  nombre:string;
+  nro:string;
+  constructor(private bdLocal:BdLocalService) { }
+  guardar(){
+    this.bdLocal.guardarContactos(this.nombre,this.nro);
+  }
   ngOnInit() {}
 
 }
